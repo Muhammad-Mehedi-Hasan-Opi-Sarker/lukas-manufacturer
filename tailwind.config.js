ef4706b91fe718e11747137574b10f0015b4e343
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-09-02 18:01:40 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#e5e7eb",
+
+          "secondary": "#eeb644",
+
+          "accent": "#37CDBE",
+
+          "neutral": "#1b1b1c",
+
+          "base-100": "#FFFFFF",
+
+          "info": "#3ABFF8",
+
+          "success": "#36D399",
+
+          "warning": "#FBBD23",
+
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}
