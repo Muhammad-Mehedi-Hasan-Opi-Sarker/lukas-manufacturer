c4bbff313a7c5315dee34cdff8badac99b9b71a1
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-09-08 18:28:51 +0600

    review

--- src/Pages/Dashboard/Reviews.js
+++ src/Pages/Dashboard/Reviews.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Reviews = () => {
+    const [user] = useAuthState(auth);
     return (
-        <div>
-            <h1>reviews</h1>
+
+        <div className='lg:px-12'>
+            <h1 className='font-bold text-3xl text-secondary'>Write Your Comment About Us</h1>
+
+            <div className="card w-96 bg-base-100 rounded-none mt-8 shadow-xl">
+                <div className="card-body">
+                    <form>
+                        <input disabled value={user.displayName} type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" /><br />
+                        <textarea className="textarea textarea-bordered w-full mt-3" placeholder="Comment"></textarea><br />
+                        <input name='rating' type="number" placeholder="Rating" className="input mt-3 input-bordered w-full max-w-xs" /><br />
+                        <input type="submit" className='btn btn-secondary w-32 rounded-full mt-2' value="Submit" />
+
+                    </form>
+                </div>
+            </div>
+
+
         </div>
     );
 };
